882b766c4250a97d54f424b2cd41f126790052fa
akivo4ka <akivavika@mail.ru>
2018-10-13 20:49:07 +0300

    Add Person

---
+++ src/Person/Main.java
@@ -0,0 +1,16 @@
+package Person;
+
+public class Main {
+    public static void main(String[] args) {
+        Person john = new Person(true, "John");
+        Person ann = new Person(false, "Ann");
+        john.marry(ann);
+        System.out.println(john.getName() + " is married to " + john.getSpouse().getName() + ".");
+        Person mary = new Person(false, "Mary");
+        john.marry(mary);
+        System.out.println("Now " + john.getName() + " is married to " + john.getSpouse().getName() + ".");
+        if (ann.getSpouse() == null) {
+            System.out.println("So " + ann.getName() + " is alone.");
+        }
+    }
+}
